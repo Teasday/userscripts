388bcce6d02452e7e5a5c9fab45ea7cb4fa99654
Teasday <osakasync@gmail.com>
2018-07-30 00:32:50 +0300

    added MFE meta

---
+++ mangadex-massfollowedit/mangadex-massfollowedit.meta.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @name         MangaDex Mass Follow Edit
+// @namespace    Teasday
+// @version      0.1
+// @description  Edits follows massively
+// @author       Teasday
+// @match        http://beta.mangadex.org/follows/manga/*
+// @icon         https://mangadex.org/favicon.ico
+// @homepageURL  https://teasday.github.io/userscripts/mangadex-massfollowedit/
+// @updateURL    https://raw.githubusercontent.com/teasday/userscripts/master/mangadex-massfollowedit/mangadex-massfollowedit.meta.js
+// @downloadURL  https://raw.githubusercontent.com/teasday/userscripts/master/mangadex-massfollowedit/mangadex-massfollowedit.user.js
+// @grant        none
+// ==/UserScript==
--- mangadex-massfollowedit/mangadex-massfollowedit.user.js
+++ mangadex-massfollowedit/mangadex-massfollowedit.user.js
@@ -5,6 +5,10 @@
 // @description  Edits follows massively
 // @author       Teasday
 // @match        http://beta.mangadex.org/follows/manga/*
+// @icon         https://mangadex.org/favicon.ico
+// @homepageURL  https://teasday.github.io/userscripts/mangadex-massfollowedit/
+// @updateURL    https://raw.githubusercontent.com/teasday/userscripts/master/mangadex-massfollowedit/mangadex-massfollowedit.meta.js
+// @downloadURL  https://raw.githubusercontent.com/teasday/userscripts/master/mangadex-massfollowedit/mangadex-massfollowedit.user.js
 // @grant        none
 // ==/UserScript==
 
